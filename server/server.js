4840f861d10ac91bb122d058c83df11ba3e1eca7
Merizza Khan <merizza.azwir@gmail.com>
2017-10-25 19:37:47 +0100

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -40,6 +40,20 @@ io.on('connection', (socket) => {
 //		console.log('createEmail', newEmail);
 //	});
 	
+	//Send every user a welcome text
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app',
+		craetedAt: new Date().getTime()
+	});
+	
+	//Send a message to every other user except the new user
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createdAt: new Date().getTime()
+	});
+	
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
 		io.emit('newMessage', {
@@ -47,6 +61,11 @@ io.on('connection', (socket) => {
 			text: message.text,
 			createdAt: new Date().getTime()
 		});
+//		socket.broadcast.emit('newMessage', {
+//			from: message.from,
+//			text: message.text,
+//			createdAt: new Date().getTime()
+//		});
 	});
 	
 	socket.on('disconnect', () => {
